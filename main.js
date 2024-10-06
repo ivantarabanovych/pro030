3ee6de962981fbdf4b3d2322f079dd9b089e917f
ivantarabanovych <vanjokcitet334@gmail.com>
2024-10-06 20:28:08 +0300

    q4

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-//цикл for
-const array = [10, 20, 30, 40];
-for (let i = 0; i < array.length; i++) {
-    console.log(array[i]);
-}
-//Цикл for..in
-const obj = { name: 'Somename', age: 30, city: 'Somecity' };
-for (let key in obj) {
-    console.log(key + ': ' + obj[key]);
-}
-//Цикл for..of
-const array = [10, 20, 30, 40];
-for (let value of array) {
-    console.log(value);
+const Array = [
+    "Some string",
+    5346,
+    {name: "Ivan", age: 21},
+    [17, 25, 23, 5, 6, 7],
+    true,
+    null, 
+    undefined
+];
+
+for(const item of Array){
+    console.log(item);
 }
